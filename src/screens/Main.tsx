bf2b857f2b184bd092e91b78dcdea474c0e2e4da
InoDev-TaeGwan <development.s@iuproject.me>
2021-05-06 17:37:31 +0900

    Edit swiper

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -27,18 +27,41 @@ const BoxGreen = styled.View`
   justify-content: center;
   background-color: green;
 `;
+const BoxCoral = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: coral;
+`;
+const BoxOrange = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: orange;
+`;
 
 const Main = () => {
   return (
     <Swiper
       dotColor="rgba(0,0,0,.0)"
       activeDotColor="rgba(0,0,0,.0)"
-      index={1}
       horizontal={false}
       loop={false}>
-      <BoxPink>
-        <Text>1page</Text>
-      </BoxPink>
+      <Swiper
+        dotColor="rgba(0,0,0,.0)"
+        activeDotColor="rgba(0,0,0,.0)"
+        loop={false}
+        index={1}>
+        <BoxCoral>
+          <Text>Left</Text>
+        </BoxCoral>
+        <BoxPink>
+          <Text>Main</Text>
+        </BoxPink>
+        <BoxGreen>
+          <Text>Right</Text>
+        </BoxGreen>
+      </Swiper>
       <Swiper
         dotColor="rgba(0,0,0,.0)"
         activeDotColor="rgba(0,0,0,.0)"
@@ -54,9 +77,21 @@ const Main = () => {
           <Text>Right</Text>
         </BoxGreen>
       </Swiper>
-      <BoxRed>
-        <Text>3page</Text>
-      </BoxRed>
+      <Swiper
+        dotColor="rgba(0,0,0,.0)"
+        activeDotColor="rgba(0,0,0,.0)"
+        loop={false}
+        index={1}>
+        <BoxRed>
+          <Text>Left</Text>
+        </BoxRed>
+        <BoxOrange>
+          <Text>Main</Text>
+        </BoxOrange>
+        <BoxGreen>
+          <Text>Right</Text>
+        </BoxGreen>
+      </Swiper>
     </Swiper>
   );
 };
